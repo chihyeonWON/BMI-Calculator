e4f2337bbf156bd2cea8280451ef585a339298ad
ylose <heungeob1003@gmail.com>
2023-01-05 15:01:29 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/app/obesity/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app.obesity
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: BmiMain(),
+    );
+  }
+}
+
+// 첫 번째 페이지
+class BmiMain extends StatefulWidget {
+  @override
+  _BmiMainState createState() => _BmiMainState();
+}
+
+class _BmiMainState extends State<BmiMain> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+// 두 번째 페이지
+class BmiResult extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
